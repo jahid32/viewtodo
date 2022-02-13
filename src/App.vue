663a214b266c58003b57572d8f5c64e819201eb4
Mostafizur Rahman <jahid32@gmail.com>
2022-02-14 00:26:31 +0600

    make todo component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div class="app-container">
+  <h1 class="app-title">Vuejs todos</h1>
+
+  <div class="composer">
+    <form @submit.prevent="addTodo"> 
+    <input type="text" v-model="newTodo" placeholder="Type and hit enter..." class="composer__input" />
+    </form>
+    <div class="filter">
+      <div class="filter__left">
+        <button class="filter__button filter__button--active">All ({{todosCount}})</button>
+        <button class="filter__button">Pending ({{pendingCount}})</button>
+        <button class="filter__button">Done ({{doneCount}})</button>
+      </div>
+      <div>
+        <button class="filter__button filter__button--danger">Clear</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="todos">
+    <!-- Todo start -->
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />  
+    <!-- Todo end -->
+
+  </div>
+</div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import { v4 as uuidv4 } from 'uuid';
+import Todo from './components/Todo.vue';
+import { computed } from '@vue/runtime-core';
+export default {
+  name: 'Todo App',
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: uuidv4(),
+        task: this.newTodo,
+        status: "pending",
+        createdAt: new Date().toString()
+      });
+      this.newTodo = '';
+    }
+  },
+  computed: {
+    todosCount() {
+      return this.todos.length;
+    },
+    pendingCount() {
+      return this.todos.filter(todo => todo.status === 'pending').length;
+    },
+    doneCount() {
+      return this.todos.filter(todo => todo.status === 'done').length;
+    }
+  }
 }
-</style>
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+   <template>
+    <div class="todo" :class="{'todo--done':todo.status == 'done'}" :key="todo.id">
+      <div class="todo__status-dot"></div>
+      <div>
+        <p class="todo__content">{{todo.task}}</p>
+        <small class="todo__time"> {{todo.createdAt}}</small>
+      </div>
+    </div>
+    </template>
+    <script>
+   export default {
+       props: ['todo'],
+   }
+   
+    </script>
+    
